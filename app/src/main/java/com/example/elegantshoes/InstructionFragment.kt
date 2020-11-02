01a4f042966f4f696d3062b236fb92d89be6ced2
moath farhoud <moathf123@gmail.com>
2020-11-02 17:02:29 +0200

    setting up instruction screen

---
+++ app/src/main/java/com/example/elegantshoes/InstructionFragment.kt
@@ -0,0 +1,32 @@
+package com.example.elegantshoes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.elegantshoes.databinding.FragmentInstructionBinding
+
+
+class InstructionFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_instruction,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+}
