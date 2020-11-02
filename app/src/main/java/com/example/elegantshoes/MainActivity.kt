8633c859dcbc4452af00fdc1a0e8db4de8ad820b
moath farhoud <moathf123@gmail.com>
2020-11-02 11:06:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elegantshoes/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.elegantshoes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+
+    }
+}
